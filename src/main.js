96d852ddb40e86ce5e861145c98494c0ad543b82
pierre.berry <pierre.berry@scalian.com>
2017-09-07 17:52:45 +0200

    Inclusion de l'interface bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 
 $(function(){
-
     var melangerPaquet = function(paquet){
         for (var i = paquet.length - 1; i > 0; i--) {
             var j = Math.floor(Math.random() * (i + 1));
@@ -11,20 +10,58 @@ $(function(){
         return paquet;
     }
 
-    // Construction du deck
-    var deck = [];
 
-    for(var i in deckInit) {
-        var carteJson = deckInit[i];
-        var nouvelleCarte = {
-            "clan": carteJson.clan,
-            "effet": carteJson.effet,
-            "id" : carteJson.id,
-            "isHidden": true
-        };
-        deck.push(nouvelleCarte);
+    var deplacerCartes = function(paquetDepart, paquetArrivee, cartes){
+        for(var i in cartes){
+            var carte = cartes[i];
+            var carteTrouvee = false;
+            var cpt = 0;
+            while(carteTrouvee){
+                if(paquetDepart[cpt] === carte.id){
+                    carteTrouvee = true;
+                    paquetDepart.splice(cpt, 1);
+                    paquetArrivee.push(carte);
+                }
+                cpt++;
+            }
+        }
+        return {
+            paquetDepart : paquetDepart,
+            paquetArrivee : paquetArrivee
+        }
     }
-    melangerPaquet(deck);
+
+    // Construction du deck
+    const deck = {
+        paquet : [],
+        initialisationPaquet: function(deckInit){
+            for(var i in deckInit) {
+                var carteJson = deckInit[i];
+                var nouvelleCarte = {
+                    "clan": carteJson.clan,
+                    "effet": carteJson.effet,
+                    "id" : carteJson.id,
+                    "isHidden": true
+                };
+                this.paquet.push(nouvelleCarte);
+            }
+            melangerPaquet(this.paquet);
+        },
+        piocherCartes : function(nombreCartes) {
+            var cartesPiochees = [];
+            if (nombreCartes < this.paquet.length) {
+                for (var i = 0; i < nombreCartes; i++) {
+                    cartesPiochees.push(this.paquet.pop());
+                }
+            } else {
+                cartesPiochees.push(this.paquet);
+                this.paquet = [];
+            }
+            return cartesPiochees;
+        }
+    };
+
+    deck.initialisationPaquet(deckInit);
 
     // Initialisation du plateau
 
@@ -32,25 +69,68 @@ $(function(){
         deck: deck
     }
 
-    for(var i in deckInit) {
+
+    // Initialisation des joueurs
+    const joueur1 = {
+        id : "joueur1",
+        village: [],
+        main: deck.piocherCartes(5)
+    }
+
+    const joueur2 = {
+        id : "joueur2",
+        village: [],
+        main: deck.piocherCartes(5)
+    }
+
+    const joueur3 = {
+        id : "joueur3",
+        village: [],
+        main: deck.piocherCartes(5)
+    }
+
+    const joueur4 = {
+        id : "joueur4",
+        village: [],
+        main: deck.piocherCartes(5)
+    }
+
+
+    for(var i in deck.paquet) {
         $("#zoneDeck").append(
             '<span class="">'
-            + plateau.deck[i].id
+            + deck.paquet[i].id
             + ' </span>'
         );
     }
 
-     var piocherCartes = function(nombreCartes) {
-        var cartesPiochees = [];
-        if (nombreCartes > this.deck.size) {
-            for (var i = 0; i < nombreCartes; i++) {
-                cartesPiochees.push(this.deck.pop);
-            }
-        } else {
-            cartesPiochees.push(this.deck);
-            this.deck = [];
-        }
-        return cartesPiochees;
+    for(var i in joueur1.main) {
+        $("#mainJoueur1").append(
+            '<span class="">'
+            + joueur1.main[i].id
+            + ' </span>'
+        );
+    }
+    for(var i in joueur2.main) {
+        $("#mainJoueur2").append(
+            '<span class="">'
+            + joueur2.main[i].id
+            + ' </span>'
+        );
+    }
+    for(var i in joueur3.main) {
+        $("#mainJoueur3").append(
+            '<span class="">'
+            + joueur3.main[i].id
+            + ' </span>'
+        );
+    }
+    for(var i in joueur4.main) {
+        $("#mainJoueur4").append(
+            '<span class="">'
+            + joueur4.main[i].id
+            + ' </span>'
+        );
     }
 
 
